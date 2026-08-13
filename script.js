3ed2924cab1f6c28e24a23158fcfc176394854bb
jomitreacy0-cmd <jomitreacy0@gmail.com>
2026-08-13 14:25:21 +0100

    updated the second fetch

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-const themeBtn = document.getElementById("themeBtn");
+const container = document.getElementById("countries-container");
 
-themeBtn.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
+fetch("https://countries.dev/countries?fields=name,capital,region,population,flag&limit=12")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Could not fetch countries");
+        }
 
-    if (document.body.classList.contains("dark-mode")) {
-        themeBtn.textContent = "☀️ Light Mode";
-    } else {
-        themeBtn.textContent = "🌙 Dark Mode";
-    }
-});
+        return response.json();
+    })
+    .then(countries => {
+
+        countries.forEach(country => {
+
+            const card = document.createElement("div");
+
+            card.classList.add("card");
+
+            card.innerHTML = `
+                <div class="flag">${country.flag || "🌍"}</div>
+
+                <h2>${country.name}</h2>
+
+                <p><strong>Capital:</strong> ${country.capital || "Not available"}</p>
+
+                <p><strong>Region:</strong> ${country.region || "Not available"}</p>
+
+                <p><strong>Population:</strong> ${
+                    country.population
+                        ? country.population.toLocaleString()
+                        : "Not available"
+                }</p>
+            `;
+
+            container.appendChild(card);
+        });
+
+    })
+    .catch(error => {
+        console.error("Error:", error);
+
+        container.innerHTML = `
+            <p>Unable to load countries.</p>
+        `;
+    });
